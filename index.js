e0927ef1b1a0be1feba25648ae4f673a754ecf02
Beth Damato <bethadamato@gmail.com>
2018-06-25 12:08:30 -0400

    adding form input and authentication structure

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const Todo = require('./db');
 
 
@@ -25,15 +28,37 @@ app.get('/', (req, res) => {
     .catch((error) => { console.log(error); });
 });
 
-app.get('/:id', (req, res) => {
-  Todo.getOne(req.params.id)
+
+app.get('/new', (req, res) => {
+  console.log('This is the /new route');
+  res.render('todo-create-page');
+});
+
+app.post('/new', (req, res) => {
+  console.log(req.body);
+  // res.send('hey you submitted the form');
+
+  Todo.add(req.body.title)
     .then((data) => {
       console.log(data);
       res.send(data);
-      // res.render('todo-detail-page', data);
+    })
+
+});
+
+
+
+app.get('/:id', (req, res) => {
+  console.log('This is the /:id route');
+  Todo.getOne(req.params.id)
+    .then((data) => {
+      console.log(data);
+      // res.send(data);
+      res.render('todo-detail-page', data);
     })
     .catch((error) => { console.log(error); });
 });
+
 
 app.listen(3000, () => {
   console.log('Your server is running!');
